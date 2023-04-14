b861cdc0df42f83f52317b1d0edf48476c21417b
Håvard <norvang123@gmail.com>
2023-04-14 18:20:38 +0200

    added sliders to update rows and columns

--- javascript.js
+++ javascript.js
@@ -1,15 +1,21 @@
 const DEFAULT_ROWS = 10;
 const DEFAULT_COLUMNS = 10;
-const MAX = 100;
+const SLIDER_MAX = 50;
+const SLIDER_MIN = 1;
 
-function createGrid(gridHeight, gridLength) {
+let currentRows = DEFAULT_ROWS;
+let currentColumns = DEFAULT_COLUMNS;
+
+
+function updateGrid(rows, columns) {
+
   const gridContainer = document.getElementById("grid-container");
   gridContainer.innerHTML = "";
-  gridContainer.style.gridTemplateRows = `repeat(${gridHeight}, 0fr)`;
-  gridContainer.style.gridTemplateColumns = `repeat(${gridLength}, 0fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${rows}, 0fr)`;
+  gridContainer.style.gridTemplateColumns = `repeat(${columns}, 0fr)`;
   let cellCount = 1;
-  for (let i = 0; i < gridHeight; i++) {
-    for (let j = 0; j < gridLength; j++) {
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
       cell.textContent = cellCount++;
@@ -26,4 +32,35 @@ function createGrid(gridHeight, gridLength) {
     }
   }
 }
-createGrid(DEFAULT_ROWS, DEFAULT_COLUMNS)
+
+function initGrid(rows, columns) {
+  const rowSlider = document.getElementById("rowSlider")
+  const columnSlider = document.getElementById("columnSlider")
+  const rowValue = document.getElementById("rowValue");
+  const columnValue = document.getElementById("columnValue");
+
+
+
+  rowSlider.addEventListener("input", (event) => {
+    currentRows = event.target.value;
+    updateGrid(event.target.value, currentColumns)
+    rowValue.textContent = currentRows;
+  })
+
+  columnSlider.addEventListener("input", (event) => {
+    currentColumns = event.target.value;
+    updateGrid(currentRows, event.target.value)
+    columnValue.textContent = currentColumns;
+  })
+  rowSlider.value = DEFAULT_ROWS;
+  rowSlider.max = SLIDER_MAX;
+  columnSlider.value = DEFAULT_COLUMNS;
+  columnSlider.max = SLIDER_MAX;
+  rowValue.textContent = DEFAULT_ROWS;
+  columnValue.textContent = DEFAULT_COLUMNS;
+
+  updateGrid(rows, columns)
+
+}
+
+initGrid(DEFAULT_ROWS, DEFAULT_COLUMNS)
