68d1b8b01bf81e3e3f5057e50f63e6d2cca98cd6
Håvard <norvang123@gmail.com>
2023-04-14 17:34:40 +0200

    Removed unused code

--- javascript.js
+++ javascript.js
@@ -2,22 +2,10 @@ const DEFAULT_ROWS = 10;
 const DEFAULT_COLUMNS = 10;
 const MAX = 100;
 
-function createGridWithPrompt() {
-  const numRowCells = prompt("How many rows do you want to make?");
-  const numRowCellsInt = parseInt(numRowCells);
-  
-  const numColumnCells = prompt("How many columns do you want to make?");
-  const numColumnCellsInt = parseInt(numColumnCells);
-  
-  const gridHeight = numRowCellsInt;
-  const gridLength = numColumnCellsInt;
-  createGrid(gridHeight, gridLength);
-}
-
 function createGrid(gridHeight, gridLength) {
   const gridContainer = document.getElementById("grid-container");
   gridContainer.innerHTML = "";
-  gridContainer.style.gridTemplateRows = `repeat(${gridHeight}, 0fr)`; 
+  gridContainer.style.gridTemplateRows = `repeat(${gridHeight}, 0fr)`;
   gridContainer.style.gridTemplateColumns = `repeat(${gridLength}, 0fr)`;
   let cellCount = 1;
   for (let i = 0; i < gridHeight; i++) {
@@ -29,16 +17,12 @@ function createGrid(gridHeight, gridLength) {
       gridContainer.appendChild(cell);
 
       cell.addEventListener("mouseover", () => {
-        
+
         const r = Math.floor(Math.random() * 256)
         const b = Math.floor(Math.random() * 256)
         const g = Math.floor(Math.random() * 256)
-        cell.style.backgroundColor = `rgb(${r }, ${b }, ${g})`;
+        cell.style.backgroundColor = `rgb(${r}, ${b}, ${g})`;
       });
-      
-    //  cell.addEventListener("mouseout", () => {
-    //    cell.style.backgroundColor = "grey";
-    //   });
     }
   }
 }
